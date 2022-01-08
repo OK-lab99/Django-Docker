cae6f3c361efbe04602605cfa61e52b2976eeaad
OKlab99 <yuukideluxe007@yahoo.co.jp>
2022-01-08 18:26:56 +0900

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -178,7 +178,7 @@ EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 # --- Gmail 送信設定 --- 
 
 
-#DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ['CLOUD_NAME'],
